5cc4321d88288acf838b90cc0f3afaeafefeb8f1
Omar Rodriguez <omarrodriguez@Omars-MacBook-Pro.local>
2023-10-20 13:23:49 -0600

    fixing build issues

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,10 @@
 const createProxyMiddleware = require("http-proxy-middleware");
-export const appUrl = process.env.APP_URL;
 require("dotenv").config();
 
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
-      target: `${appUrl}:${process.env.BACKEND_PORT}`,
+      target: `https://appqa.vercel.app:${process.env.BACKEND_PORT}`,
       changeOrigin: true,
       logLevel: "debug",
     })
